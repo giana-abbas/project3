9a87054417b6e62b35ae0ceba9f4ce0949fc89df
jpark71 <jpark71@villanova.edu>
2021-12-02 12:26:09 -0500

    updated mainactivity.java,secondactivity.java,androidmanifest.xml,dictionarydetail.xml

--- DictionaryDetailActivity.java
+++ DictionaryDetailActivity.java
@@ -1,7 +1,10 @@
 package com.example.project3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,12 +17,17 @@ public class DictionaryDetailActivity extends AppCompatActivity {
     private final LinkedList<String> mDictionaryDefs = new LinkedList<>();
     private RecyclerView mRecyclerView;
     private DictionaryAdapter mAdapter;
+    private TextView mDisplaySearchedWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionarydetail);
-
+        Intent intent = getIntent();
+        String wordReceived = intent.getStringExtra("WORD");
+        Log.d("SECONDACTIVITY", wordReceived);
+        mDisplaySearchedWord = findViewById(R.id.searchedWord);
+        mDisplaySearchedWord.setText(wordReceived);
 
         //get example word from first activity
         //add to linked lists using API -
--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.a2053project3;
+package com.example.project3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -13,12 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mWordInput = (EditText) findViewById(R.id.wordInput);
+        mWordInput = findViewById(R.id.wordInput);
     }
 
     public void searchWords(View view){
         String queryString = mWordInput.getText().toString();
-
+        Intent intent = new Intent (this, DictionaryDetailActivity.class);
+        intent.putExtra("WORD", queryString);
+        startActivity(intent);
     }
 
 }
